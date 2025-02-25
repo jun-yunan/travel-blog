aa9e59b7143bbb440ffab69045893b3cfda4a9cc
devryunguyen <devryunguyen@gmail.com>
2025-02-25 15:07:04 +0700

    test

--- system/services/Logger.php
+++ system/services/Logger.php
@@ -149,7 +149,7 @@ class Logger
 
     // Nếu thư mục logs chưa tồn tại, tạo nó
     if (!is_dir($logDirectory)) {
-        mkdir($logDirectory, 0777, true); // true để tạo thư mục con nếu cần
+        mkdir($logDirectory, 0777, true); // true để tạo thư mục con nếu khong can
     }
 
     // Kiểm tra và tạo file nếu chưa có
