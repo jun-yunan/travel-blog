d1eddd7de5c0c6387db7a6cf1e7a12d938d230d4
TrungNhan003 <thaitrungnhan02@gmail.com>
2025-02-26 21:38:07 +0700

    Thêm footer

--- src/common/view/Footer.php
+++ src/common/view/Footer.php
@@ -10,20 +10,40 @@
             ?> -->
 
     <br><br>
+    <footer style="background-color: #0057b8; color: white; padding: 20px 0;">
+    <div class="container">
+        <style>
+    footer {
+        font-size: 15px;
+    }
+</style>
 
-    <footer class="page-footer teal" style="padding-top: 10px;">
-
-        <div class='container'>
-            <h4>Footer</h4>
-        </div>
-        <div class="footer-copyright">
-            <div class="container">
-                Footer:
-                src/view/template/common/footer.php
+        </style>
+        <div class="row">
+            <div class="col-md-6">
+                <h6><strong>&copy; Nhóm 2.5 Lập trình web với PHP và MySQL</strong></h6>
+                <p><strong>Lớp: </strong> Công Nghệ Thông Tin 16B</p>
+                <p><strong>Tên đề tài/dự án: </strong>Blog Travel</p>
+                <p><strong>Điện thoại: </strong>0794251534 (Nhóm trưởng: Nhựt Trường)</p>
+                <p><strong>Email:</strong>nntruong-cntt16@tdu.edu.vn</a></p>
             </div>
+            <div class="col-md-6">
+                <h6><strong>Thành viên nhóm</strong></h6>
+                <ul style="list-style: none; padding-left: 0;">
+                    <li><a href="#" style="color: white; text-decoration: none;">217060141	Nguyễn Nhựt Trường</a></li>
+                    <li><a href="#" style="color: white; text-decoration: none;">217060140	Thái Trung Nhẫn</a></li>
+                    <li><a href="#" style="color: white; text-decoration: none;">217060145	Nguyễn Anh Kiệt</a></li>
+                    <li><a href="#" style="color: white; text-decoration: none;">217060103	Lâm Phan Kiều Trinh</a></li>
+                    <li><a href="#" style="color: white; text-decoration: none;">217060155	Lê Thị Kim Xuyến</a></li>
+                    <li><a href="#" style="color: white; text-decoration: none;">217060150	Trần Tiến Phát</a></li>
+                    <li><a href="#" style="color: white; text-decoration: none;">217060146	Tăng Thị Thuý Huỳnh</a></li>
+                </ul>
+            </div>
+            
         </div>
+    </div>
+</footer>
 
-    </footer>
 
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
