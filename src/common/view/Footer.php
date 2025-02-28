d26c2bf0f18c76e586eb5c7554111391fd66adea
TrungNhan003 <thaitrungnhan02@gmail.com>
2025-02-28 17:13:27 +0700

    Sua lai footer

--- src/common/view/Footer.php
+++ src/common/view/Footer.php
@@ -1,53 +1,56 @@
-    <!-- </div> -->
-    </div>
-
-    <!-- Console -->
-    <!-- <?php
-
-            // if ($this->is_debug_console_enabled) {
-            //     include "src/common/view/Console.php";
-            // }
-            ?> -->
-
-    <br><br>
-    <footer style="background-color: #0057b8; color: white; padding: 20px 0;">
+<footer style="background-color: #0057b8; color: white; padding: 40px 0;">
     <div class="container">
-        <style>
-    footer {
-        font-size: 15px;
-    }
-</style>
-
-        </style>
         <div class="row">
+            <!-- Thông tin liên hệ -->
+            <div class="col-md-4">
+                <h6><strong>THÔNG TIN LIÊN HỆ</strong></h6>
+                <p><strong>CÔNG TY CỔ PHẦN TRAVELBLOGS</strong></p>
+                <p>Địa chỉ: P. Lê Bình, Q. Cái Răng, Tp. Cần Thơ, Việt Nam</p>
+                <p>Hỗ trợ: 079425xxxx</p>
+            </div>
+
+            <!-- Chăm sóc khách hàng -->
+            <div class="col-md-4">
+                <h6><strong>CHĂM SÓC KHÁCH HÀNG</strong></h6>
+                <p>Thời gian hỗ trợ: 8H - 22H Hằng ngày</p>
+                <p>Hotline: 079425xxxx</p>
+            </div>
+
+            <!-- Phương thức thanh toán -->
+            <div class="col-md-4">
+                <h6><strong>PHƯƠNG THỨC THANH TOÁN</strong></h6>
+                <div style="display: flex; gap: 10px; align-items: center;">
+                    <img src="/assets/images/shopeepay.jpg" alt="ShopeePay" width="60" height="40">
+                    <img src="/assets/images/mastercard.jpg" alt="Ngân Hàng" width="40" height="40">
+                    <img src="/assets/images/zalopay.jpg" alt="ZaloPay" width="40" height="40">
+                </div>
+            </div>
+        </div>
+
+        <hr style="border-color: rgba(255,255,255,0.3);">
+
+        <!-- Thông tin nhóm -->
+        <div class="row mt-3">
             <div class="col-md-6">
                 <h6><strong>&copy; Nhóm 2.5 Lập trình web với PHP và MySQL</strong></h6>
                 <p><strong>Lớp: </strong> Công Nghệ Thông Tin 16B</p>
                 <p><strong>Tên đề tài/dự án: </strong>Blog Travel</p>
-                <p><strong>Điện thoại: </strong>0794251534 (Nhóm trưởng: Nhựt Trường)</p>
-                <p><strong>Email:</strong>nntruong-cntt16@tdu.edu.vn</a></p>
+                <p><strong>Điện thoại: </strong>0794251534 (Nhựt Trường)</p>
+                <p><strong>Email:</strong> nntruong-cntt16@tdu.edu.vn</p>
             </div>
+
             <div class="col-md-6">
                 <h6><strong>Thành viên nhóm</strong></h6>
                 <ul style="list-style: none; padding-left: 0;">
-                    <li><a href="#" style="color: white; text-decoration: none;">217060141	Nguyễn Nhựt Trường</a></li>
-                    <li><a href="#" style="color: white; text-decoration: none;">217060140	Thái Trung Nhẫn</a></li>
-                    <li><a href="#" style="color: white; text-decoration: none;">217060145	Nguyễn Anh Kiệt</a></li>
-                    <li><a href="#" style="color: white; text-decoration: none;">217060103	Lâm Phan Kiều Trinh</a></li>
-                    <li><a href="#" style="color: white; text-decoration: none;">217060155	Lê Thị Kim Xuyến</a></li>
-                    <li><a href="#" style="color: white; text-decoration: none;">217060150	Trần Tiến Phát</a></li>
-                    <li><a href="#" style="color: white; text-decoration: none;">217060146	Tăng Thị Thuý Huỳnh</a></li>
+                    <li>217060141 - Nguyễn Nhựt Trường</li>
+                    <li>217060140 - Thái Trung Nhẫn</li>
+                    <li>217060145 - Nguyễn Anh Kiệt</li>
+                    <li>217060103 - Lâm Phan Kiều Trinh</li>
+                    <li>217060155 - Lê Thị Kim Xuyến</li>
+                    <li>217060150 - Trần Tiến Phát</li>
+                    <li>217060146 - Tăng Thị Thuý Huỳnh</li>
                 </ul>
             </div>
-            
         </div>
     </div>
 </footer>
-
-
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    </body>
-
-    </html>
